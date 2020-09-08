69c7cece655e7e8d78c17b1cb1cfcb5439361def
cemcoe <chemxcode@gmail.com>
2020-09-08 11:28:55 +0800

    build: 配置开发环境和线上环境接口地址

--- src/network/request.js
+++ src/network/request.js
@@ -1,14 +1,27 @@
 import axios from 'axios'
 axios.defaults.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem('token')}` || ''
 
+// 多环境配置
+let apiUrl
+if (process.env.NODE_ENV == 'production') {
+  //线上
+  apiUrl = 'http://demo.cemcoe.com/jianshu_api'
+} else if (process.env.NODE_ENV == 'development') {
+  //本地
+  apiUrl = 'http://localhost:3000'
+} else {
+  //预发布
+  apiUrl = 'http://localhost:3000'
+}
+
 export function request(config) {
   // 1. 创建axios实例
   const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: apiUrl,
     timeout: 10000, // 设置超时时间10s
   })
   // 2. 请求拦截器，添加jwt token
-  
+
   instance.interceptors.request.use(config => {
     // config.headers.Authorization = `Bearer ${sessionStorage.getItem('token')}` || ''
     return config
