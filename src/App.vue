ecfbe538b925e6277ba8e994dda54abb5ba68593
cemcoe <chemxcode@gmail.com>
2020-09-09 21:41:53 +0800

    feat: tabbar组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
     <router-view/>
+    <main-tab-bar class="main-tab-bar"></main-tab-bar>
   </div>
 </template>
+
+<script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+export default {
+  components: {
+    MainTabBar,
+  }
+}
+</script>
 
 <style>
 </style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/following">
+      <img slot="item-icon" src="~assets/img/tabbar/following.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/following_active.svg">
+      <div slot="item-text">关注</div>
+    </tab-bar-item>
+    <tab-bar-item path="/island">
+      <img slot="item-icon" src="~assets/img/tabbar/island.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/island_active.svg">
+      <div slot="item-text">小岛</div>
+    </tab-bar-item>
+    <tab-bar-item path="/message">
+      <img slot="item-icon" src="~assets/img/tabbar/message.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/message_active.svg">
+      <div slot="item-text">消息</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBarItem from "components/common/tabbar/TabBarItem";
+import TabBar from "components/common/tabbar/TabBar";
+
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
+</script>
+
+<style scoped>
+</style>
