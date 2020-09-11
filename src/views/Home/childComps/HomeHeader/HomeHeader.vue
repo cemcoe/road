954d5c5afd52697e515e2be1ac56300b227d42be
cemcoe <chemxcode@gmail.com>
2020-09-11 10:23:56 +0800

    refactor: HomeHeader

--- src/views/Home/childComps/HomeHeader/HomeHeader.vue
+++ src/views/Home/childComps/HomeHeader/HomeHeader.vue
@@ -2,76 +2,84 @@
   <div class="container">
     <div class="home-header">
       <div class="left logo">
-        <a href="/">
+        <router-link to="/">
           <img src="./imgs/nav-logo.png" alt="logo" />
-        </a>
+        </router-link>
       </div>
       <div class="center">
         <input class="search" type="text" placeholder="搜索感兴趣的内容" />
-        {{user}}
       </div>
-      <div class="right">
-        <span v-if="user.name" class="user" @click="goto_profile">{{user.name}}</span>
-        <!-- <a v-else class="user" href="/login">登录</a> -->
-        <router-link v-else class="user" :to="'/login'" replace>登录</router-link>
-        
+      <div class="right user">
+        <!-- <div v-if="user.name" @click="goto_profile" class="user-info">{{user.name}}</div> -->
+        <!-- 用户登录后用户详情页的链接是不变的，使用计算属性更好 -->
+        <router-link v-if="user.name" :to="user_detail_link" class="user-info">{{user.name}}</router-link>
+        <router-link v-else :to="'/login'" replace class="login">登录|注册</router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
+  name: "HomeHeader",
   methods: {
-    goto_profile() {
-      this.$router.push("/u/" + this.$store.state.user._id);
-    },
+    // goto_profile() {
+    //   this.$router.push("/u/" + this.$store.state.user._id);
+    // },
   },
   computed: {
-    user () {
-      return this.$store.state.user
-    }
-  }
+    user() {
+      return this.$store.state.user;
+    },
+    user_detail_link() {
+      return "/u/" + this.$store.state.user._id;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .container {
   width: 100%;
-  height: 70px;
+  height: 50px;
 }
+
 .home-header {
   display: flex;
   align-items: center;
   margin: 0 auto;
-  /* background-color: #000; */
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.2);
 }
+
 img {
-  width: 100px;
-  height: 56px;
+  height: 50px;
 }
+
 .home-header .left {
   flex: 0 0 100px;
   display: flex;
   align-items: center;
 }
+
 .home-header .center {
   flex: 1;
 }
-.home-header .right {
-  flex: 0 0 60px;
-  display: flex;
-  
-}
-.home-header .right .user {
-  height: 60px;
-  line-height: 60px;
-  /* padding-right: 14px; */
-  border-radius: 50%;
-  margin-right: 14px;
-  background-color: rgb(243, 239, 241);
-  box-shadow: 0 0 4px rgb(247, 244, 244);
 
+.home-header .right {
+  flex: 0 0 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
 }
+
+.home-header .right .user-info {
+  background-color: rgb(232, 232, 243);
+  height: 50px;
+  width: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: #f40;
+}
+
 </style>
