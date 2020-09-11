b7f4ee745867cd112fa45bdbb69e3c3ef8d1d178
cemcoe <chemxcode@gmail.com>
2020-09-11 11:46:26 +0800

    refactor: PostList

--- src/views/Home/childComps/PostList/PostList.vue
+++ src/views/Home/childComps/PostList/PostList.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="container">
     <div v-for="item in postList" :key="item._id" class="post">
-      <div class="title">
+      <div class="post-item">
+        <div class="title">
         <router-link :to="'/p/' + item._id">
         {{item.title}}
         </router-link>
       </div>
-      <div class="content">{{item.content}}</div>
-      <div class="info">
+      <div class="abstract">{{item.content}}</div>
+      <div class="meta">
         <!-- <a :href="'/u/' + item.author._id">{{item.author.name}}</a> -->
-        <router-link :to="'/u/' + item.author._id">{{item.author.name}}</router-link>
+        <router-link :to="'/u/' + item.author._id" class="nickname">{{item.author.name}}</router-link>
+        <span class="creat-date">{{item.createdAt}}</span>
       </div>
-    </div>
+    </div>  
+
+      </div>
+      
     <button @click="loadmore">加载更多</button>
 
   </div>
@@ -43,20 +48,38 @@ export default {
 
 <style scoped>
 .container {
-  margin-top: 80px;
+  /* 测试 */
+  margin-top: 20px;
+  margin-bottom: 80px;
 }
-.post {
-  height: 120px;
-  margin-bottom: 20px;
-  overflow: hidden;
+
+
+
+.post-item {
+  padding: 12px 8px;
+  box-sizing: content-box;
+  margin-bottom: 12px;
+  background-color: rgb(248, 245, 245);
+
 }
 .title {
   font-weight: 700;
 }
-.content {
+.abstract {
   font-size: 16px;
   color: rgb(97, 101, 105);
   height: 60px;
   overflow: hidden;
+  padding-top: 12px;
+  padding-bottom: 12px;
 }
+.meta {
+  font-size: 14px;
+}
+
+.meta .nickname, .creat-date {
+  color: #b4b4b4;
+  padding-right: 12px;
+};
+
 </style>
