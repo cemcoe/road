de6f9ec61646bae3143f0387fe94f2935d1c889a
cemcoe <chemxcode@gmail.com>
2020-09-14 07:47:05 +0800

    feat: user界面关注和取消关注逻辑的实现

--- src/network/user.js
+++ src/network/user.js
@@ -6,3 +6,55 @@ export function getUserPosts(id) {
     url: `/users/${id}/posts`,
   })
 }
+
+// 关注某人
+// router.put('/users/following/:id',auth, follow)
+export function followingUser(id) {
+  return request({
+    method: "put",
+    url: `/users/following/${id}`,
+    params: {
+      id
+    }
+  })
+}
+
+// 取关某人
+// router.get('/users/:id/following', listFollowing)
+// router.put('/users/following/:id',auth, follow)
+// router.delete('/users/following/:id', auth, unfollow)
+// router.get('/users/:id/followers', listFollowers)
+export function unfollowingUser(id) {
+  return request({
+    method: "delete",
+    url: `/users/following/${id}`,
+    params: {
+      id
+    }
+  })
+}
+
+// 获取关注列表
+// router.get('/users/:id/following', listFollowing)
+export function listfollowingUser(id) {
+  return request({
+    method: "get",
+    url: `/users/${id}/following`,
+    params: {
+      id
+    }
+  })
+}
+
+
+// 获取粉丝列表
+// router.get('/users/:id/followers', listFollowers)
+export function listfollower(id) {
+  return request({
+    method: "get",
+    url: `/users/${id}/followers`,
+    params: {
+      id
+    }
+  })
+}
--- src/views/User/User.vue
+++ src/views/User/User.vue
@@ -9,6 +9,18 @@
         {{user.name}}
         {{user.gender}}
       </div>
+      <button>简信</button>
+      <button @click="followingUser">关注</button>
+      <button @click="unfollowingUser">取消关注</button>
+      <button>礼物</button>
+
+      <div class="follow">
+        <span>{{followingUsers.length}} 关注</span> ||
+        <span>{{followers.length}} 粉丝</span>
+      </div>
+      <hr />
+
+      <div>简书优秀创作者</div>
 
       <!-- 用户文章列表 -->
       <div class="user-posts">
@@ -21,7 +33,13 @@
 
 <script>
 import { getUserInfo } from "network/profile.js";
-import { getUserPosts } from "network/user.js";
+import {
+  getUserPosts,
+  followingUser,
+  unfollowingUser,
+  listfollowingUser,
+  listfollower,
+} from "network/user.js";
 
 import PostList from "components/content/postList/PostList";
 
@@ -33,12 +51,14 @@ export default {
       // 用户信息
       user: {},
       userPosts: [],
+      followingUsers: [], // 关注
+      followers: [], // 粉丝
     };
   },
   components: {
     PostList,
-
   },
+
   // 有id向服务器发请求
   // 拿到路由传来的参数
   created() {
@@ -55,9 +75,42 @@ export default {
     });
     // 获取用户的文章列表
     getUserPosts(this._id).then((res) => {
-      console.log(res);
-      this.userPosts = res.data
+      // console.log(res);
+      this.userPosts = res.data;
     });
+    // 获取用户的关注列表
+    listfollowingUser(this._id).then((res) => {
+      // console.log(res);
+      // this.userPosts = res.data;
+      this.followingUsers = res.data.following;
+    });
+    // 获取用户粉丝列表
+    listfollower(this._id).then((res) => {
+      // console.log(res)
+      this.followers = res.data.followers;
+    });
+  },
+  methods: {
+    followingUser() {
+      // 关注当前用户
+      followingUser(this._id).then((res) => {
+        if (res.status === 401) {
+          console.log("关注失败");
+          return;
+        }
+        console.log("关注成功");
+      });
+    },
+    unfollowingUser() {
+      // 取消当前用户
+      unfollowingUser(this._id).then((res) => {
+        if (res.status === 401) {
+          console.log("取消关注失败");
+          return;
+        }
+        console.log("取消关注成功");
+      });
+    },
   },
 };
 </script>
