2792bf4c96b2ee68a652ebe75d6d4d8b29ed6306
cemcoe <chemxcode@gmail.com>
2020-09-19 17:49:39 +0800

    feat: ProfileHeader

--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -1,30 +1,35 @@
 <template>
   <div>
-    <!-- <div v-if="!user._id">查无此人用户id从vuex中拿登录id{{this.$store.state.user._id}}</div> -->
-    
-    <div>
-      <!-- {{user._id}} -->
+    <profile-header></profile-header>
+    <div class="user">
+      <div class="profile">
+      <img :src="user.avatar" alt="avatar">
       {{user.name}}
-      {{user.gender}}
-      <hr />
-      {{user.bio}}
-      <hr />
-      {{user.avatar}}
-      <hr />
-      关注：
-      粉丝：
-      <hr />
+    </div>
+    <div class="mine">
+      <ul>
+        <li>我的文章</li>
+        <li>我的贴子</li>
+        <li>赞和收藏</li>
+        <li>我的书架</li>
+      </ul>
+    </div>
+
+    </div>
+    
+
+    <div>
       <button @click="signout">退出登录</button>
       <hr />
       <!-- <div>
         <router-link to="/upload">上传图片</router-link>
-      </div> -->
+      </div>-->
       <div>
         <router-link :to="'/reset/'+user._id">更新用户信息</router-link>
       </div>
     </div>
     <hr />
-    
+
     <div>
       <li>我的文章</li>
       <li>我的帖子</li>
@@ -52,14 +57,17 @@
       <li>简书活动</li>
       <li>每日任务</li>
     </div>
-
   </div>
 </template>
 
 <script>
 import { getUserInfo } from "network/user";
+import ProfileHeader from "./childComps/ProfileHeader/ProfileHeader";
 export default {
   name: "Profile",
+  components: {
+    ProfileHeader,
+  },
   data() {
     return {
       // _id: this.$store.state.user._id || null,
@@ -72,34 +80,60 @@ export default {
   created() {
     // // this._id = this.$route.params.id;
     // console.log(this._id)
-    if(this.$store.state.user._id) {
+    if (this.$store.state.user._id) {
       getUserInfo(this.$store.state.user._id).then((res) => {
-      // 错误处理
-      if (res.status === 404) {
-        console.log("查无此人");
-      }
-      if (res.status === 200) {
-        console.log(res);
-        this.user = res.data.user;
-      }
-    });
-
+        // 错误处理
+        if (res.status === 404) {
+          console.log("查无此人");
+        }
+        if (res.status === 200) {
+          console.log(res);
+          this.user = res.data.user;
+        }
+      });
     }
-    
   },
 
   methods: {
     signout() {
-      this.$store.commit("del_token")
-      this.$store.commit("del_user")
-      this.user = {}
-      this.$toast.show('退出登录成功' , 2000)
-
-    }
+      this.$store.commit("del_token");
+      this.$store.commit("del_user");
+      this.user = {};
+      this.$toast.show("退出登录成功", 2000);
+    },
   },
-
 };
 </script>
 
-<style>
+<style scoped>
+.user {
+  width: 90%;
+  margin: 10px auto;
+  background-color: rgb(247, 241, 241);
+  border-radius: 10px;
+  padding: 20px;
+
+
+}
+
+.mine ul {
+  display: flex;
+  justify-content: center;
+}
+
+.mine ul li{
+  flex: 1;
+  /* background-color: #000; */
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  
+
+}
+
+img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
 </style>
---
+++ src/views/Profile/childComps/ProfileHeader/ProfileHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <nav-bar>
+      <div slot="left" @click="more">相机</div>
+      <div slot="right">
+        <router-link to="/write">Write</router-link>
+      </div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+export default {
+  name: "ProfileHeader",
+  methods: {
+  },
+  created() {},
+  components: {
+    NavBar,
+  },
+  methods: {
+    more() {
+      this.$toast.show('还没写呢')
+
+    }
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+
+.container {
+  border-bottom: 8px solid #000;
+  
+}
+</style>
