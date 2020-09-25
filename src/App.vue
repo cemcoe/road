62c8b97be1f11c1d2a092dd796a0c4134d7f678e
cemcoe <chemxcode@gmail.com>
2020-09-25 20:15:49 +0800

    feat: keepalive home

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view />
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+    
     <main-tab-bar v-if="$route.meta.showFooter" class="main-tab-bar"></main-tab-bar>
   </div>
 </template>
