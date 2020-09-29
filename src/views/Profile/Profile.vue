12128591f5f7e144168e527143c145a69f369e3c
cemcoe <chemxcode@gmail.com>
2020-09-29 17:21:32 +0800

    refactor: 拆分Profile

--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -1,44 +1,8 @@
 <template>
   <div>
     <profile-header></profile-header>
-    <div class="user">
-      <div class="profile" v-if="user._id">
-        <img :src="user.avatar" alt="avatar" />
-        {{ user.name }}
-      </div>
-      <div v-else>
-        <router-link :to="'/login/'">登录</router-link>
-      </div>
-      <hr />
-      <div class="mine">
-        <ul>
-          <li>我的文章</li>
-          <li>我的贴子</li>
-          <li>赞和收藏</li>
-          <li>我的书架</li>
-        </ul>
-      </div>
-    </div>
-
-    <div>
-      <button v-if="user._id" @click="signout">退出登录</button>
-      <div v-if="user._id">
-        <router-link :to="'/reset/' + user._id">更新用户信息</router-link>
-      </div>
-    </div>
-
-    <div class="product">
-      <div>简述砖</div>
-      <div>简书会员</div>
-      <div>天天抽奖</div>
-      <div>排行榜</div>
-    </div>
-
-    <div>
-      <li>简书公告1</li>
-      <li>简书公告2</li>
-    </div>
-    <hr />
+    <user-info-banner></user-info-banner>
+    <product-banner></product-banner>
 
     <div class="li-item">
       <div>我的钱包</div>
@@ -52,16 +16,32 @@
         <router-link to="/about">关于</router-link>
       </div>
     </div>
+
+    <div>
+      <button v-if="user._id" @click="signout">退出登录</button>
+      <div v-if="user._id">
+        <router-link :to="'/reset/' + user._id">更新用户信息</router-link>
+      </div>
+    </div>
+    <div>
+      <li>简书公告1</li>
+      <li>简书公告2</li>
+    </div>
+    <hr />
   </div>
 </template>
 
 <script>
 import ProfileHeader from "./childComps/ProfileHeader/ProfileHeader";
+import UserInfoBanner from "./childComps/UserInfoBanner/UserInfoBanner";
+import ProductBanner from "./childComps/ProductBanner/ProductBanner";
 import { mapState } from "vuex";
 export default {
   name: "Profile",
   components: {
     ProfileHeader,
+    UserInfoBanner,
+    ProductBanner,
   },
   data() {
     return {};
@@ -81,54 +61,16 @@ export default {
 </script>
 
 <style scoped>
-.user {
-  width: 90%;
-
-  margin: 10px auto;
-  background-color: rgb(247, 241, 241);
-  border-radius: 10px;
-}
-
-.mine ul {
-  display: flex;
-  justify-content: center;
-}
-
-.mine ul li {
-  flex: 1;
-  /* background-color: #000; */
-  height: 100px;
-  text-align: center;
-  line-height: 100px;
-}
-
-img {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-
-.product {
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  background-color: rgb(231, 231, 238);
-}
-
-.product div {
-  flex: 1;
-  text-align: center;
-  padding: 20px;
-}
-
 .li-item {
   width: 90%;
-  margin: 0 auto;
+  margin: 10px auto;
+  box-shadow: 0px 0px 10px rgb(223, 217, 217);
+  padding: 16px;
+  border-radius: 6px;
 }
 
 .li-item div {
   padding: 10px;
-  background-color: rgb(227, 227, 238);
   margin-bottom: 4px;
 }
 </style>
