76cb2fe1a757962e8284b3924db08601c3e309fb
cemcoe <chemxcode@gmail.com>
2020-10-02 07:08:49 +0800

    refactor: 规范Home代码

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="home">
-    <div id="nav">
-      <home-header></home-header>
-      <home-tab-control></home-tab-control>
-      <keep-alive>
-        <router-view></router-view>
-      </keep-alive>
+    <home-header></home-header>
+    <home-tab-control></home-tab-control>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
 
-      <div class="write">
-        <router-link to="/write">写</router-link>
-      </div>
+    <!-- TODO：将write按钮抽离成一个content组件 -->
+    <div class="write">
+      <router-link to="/write">写</router-link>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HomeHeader from "./childComps/HomeHeader/HomeHeader";
 import HomeTabControl from "./childComps/HomeTabControl/HomeTabControl";
+
+import { mapState } from 'vuex'
 
 export default {
   name: "Home",
@@ -28,13 +28,15 @@ export default {
   data() {
     return {};
   },
-  computed: {
-    userInfo() {
-      return this.$store.state.userInfo;
-    },
-  },
   created() {
-    this.$toast.show('再次提醒，网站文章数据由狗屁不同文章生成器生成，不具有任何的价值，仅供占位使用。亦不代表本人观点。', 4000)
+    this.$toast.show(
+      "再次提醒，网站文章数据由狗屁不通文章生成器生成，不具有任何的价值，仅供占位使用。亦不代表本人观点。",
+      4000
+    );
+  },
+  computed: {
+    ...mapState(['userInfo']),
+    
   },
 };
 </script>
