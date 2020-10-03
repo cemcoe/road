7e915f204276c1b2727afe6c86ce757ce59764b6
cemcoe <chemxcode@gmail.com>
2020-10-03 08:06:05 +0800

    refactor: 为判断是否关注作者getters预留参数

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,10 @@
 export default {
-  isFollowingAuthor(state) {
+  isFollowingAuthor: (state) => (author = state.author) => {
     // 是否已经关注该作者
-    const { followingUsers, author } = state
+    const { followingUsers } = state
     for (let index = 0; index < followingUsers.length; index++) {
       const element = followingUsers[index];
-      if(element._id === author._id) {
+      if (element._id === author._id) {
         // console.log(element._id)
         return true
       }
