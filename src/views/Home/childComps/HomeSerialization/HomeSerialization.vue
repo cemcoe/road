e5900b5fda2d959ad6bba4892990bef0263879d6
cemcoe <chemxcode@gmail.com>
2020-10-12 11:49:41 +0800

    首页连载列表的基础展示

--- src/views/Home/childComps/HomeSerialization/HomeSerialization.vue
+++ src/views/Home/childComps/HomeSerialization/HomeSerialization.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    Serialization
-    <hr>
     <div class="items">
       <div class="item" v-for="notebook in notebooks" :key="notebook._id">
-        {{ notebook }}
-        <hr>
+        <div class="icon">
+          <img src="https://cemcoe.com/m.png" alt="icon" />
+        </div>
+        <div class="meta">
+          {{ notebook.title }}
+        </div>
+        <div class="author">
+          {{ notebook.author.name }}
+        </div>
       </div>
     </div>
   </div>
@@ -29,5 +34,40 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.items {
+  display: grid;
+  grid-template-columns: repeat(3, 30%);
+  gap: 6px;
+  justify-content: center;
+}
+.item {
+  background-color: rgb(238, 235, 243);
+  box-shadow: 0 0 4px rgb(224, 220, 220);
+  border-radius: 4px;
+}
+
+.item .icon {
+  padding-top: 6px;
+  padding-bottom: 6px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+}
+.icon img {
+  width: 80%;
+  background-color: #fff;
+  border: 1px solid #000;
+}
+
+.item .meta {
+  font-size: 16px;
+  padding: 6px;
+}
+
+.item .author {
+  padding: 6px;
+  font-size: 12px;
+  color: rgba(34, 33, 33, 0.5);
+}
 </style>
