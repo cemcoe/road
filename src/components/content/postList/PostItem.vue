1bb58baafb05a0954c733d422a0bc53b67bdeb3d
cemcoe <chemxcode@gmail.com>
2020-10-15 10:41:14 +0800

    feat: 根据文章中的图片数量采取不同展示方式

--- src/components/content/postList/PostItem.vue
+++ src/components/content/postList/PostItem.vue
@@ -5,9 +5,41 @@
         <span>{{ post.title }}</span>
       </div>
     </router-link>
-    <router-link :to="postUrl">
-      <div class="abstract">{{ post.abstract }}</div>
-    </router-link>
+
+    <section class="abstract">
+      <!-- 文章中没有图片 -->
+      <div class="no-pic" v-if="imgsLinkLength === 0">
+        <router-link :to="postUrl">
+          <div class="content">
+            <div class="text">{{ post.abstract }}</div>
+          </div>
+        </router-link>
+      </div>
+
+      <!-- 文章中有一张图片 -->
+      <div class="one-pic" v-if="imgsLinkLength === 1">
+        <router-link :to="postUrl">
+          <div class="content">
+            <div class="text">
+              {{ post.abstract }}
+            </div>
+            <img :src="imgs[0]" alt="imgs" />
+            <!-- 文章中有{{imgs.length}}张图片 -->
+          </div>
+        </router-link>
+      </div>
+
+      <!-- 文章中的图片超过三张 -->
+
+      <div class="more-pic" v-if="imgsLinkLength >= 3">
+        <router-link :to="postUrl">
+          <div class="content">
+            <img v-for="img in imgs" :src="img" alt="img" />
+          </div>
+        </router-link>
+      </div>
+    </section>
+
     <div class="meta">
       <span>
         <router-link :to="authorUrl" class="nickname">{{
@@ -38,6 +70,22 @@ export default {
     createDate() {
       return this.post.createdAt.split("T")[0];
     },
+    imgsLinkLength() {
+      return this.post.imgsLink.length;
+    },
+    imgs() {
+      // 1 => 1
+      // 2 => 1
+      // 3 => 3
+      // 4 => 3
+      if (this.post.imgsLink.length <= 2) {
+        // 取其中一项
+        return this.post.imgsLink.slice(0, 1);
+      } else if (this.post.imgsLink.length >= 3) {
+        // 取前三项
+        return this.post.imgsLink.slice(0, 3);
+      }
+    },
   },
 };
 </script>
@@ -54,17 +102,38 @@ export default {
   line-height: 24px;
   color: #000000;
 }
+
 .abstract {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.abstract .text {
   font-size: 14px;
   color: rgb(97, 101, 105);
-  margin-top: 12px;
-  margin-bottom: 12px;
+  /* margin-top: 12px;
+  margin-bottom: 12px; */
   line-height: 26px;
   /* 多行文本截断 */
   display: -webkit-box;
   overflow: hidden;
   -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
+}
+
+.one-pic .content {
+  display: flex;
+}
+
+.one-pic img {
+  width: 120px;
+}
+
+.more-pic .content {
+  display: flex;
+  justify-content: space-between;
+}
+.more-pic .content img {
+  width: 33%;
 }
 .meta {
   font-size: 14px;
