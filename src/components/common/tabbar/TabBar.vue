c2741e26fa6beda9031657faa53f9f9c85e63db8
cemcoe <chemxcode@gmail.com>
2020-10-28 14:20:03 +0800

    styles: 优化tabbar样式

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -20,13 +20,13 @@ export default {
 }
 #tab-bar {
   display: flex;
-  background-color: #f6f6f6;
+  background-color: #fff;
 
   position: fixed;
   left: 0;
   right: 0;
   bottom: 0;
 
-  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
+  box-shadow: 0 0 4px #000;
 }
 </style>
