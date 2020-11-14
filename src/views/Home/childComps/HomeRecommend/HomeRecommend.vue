ec56e147e8babf49af8799e2b62d677cd8f2f3fd
cemcoe <chemxcode@gmail.com>
2020-11-14 11:02:53 +0800

    fix: 本页已经没有数据，下页还向服务器请求数据的问题

--- src/views/Home/childComps/HomeRecommend/HomeRecommend.vue
+++ src/views/Home/childComps/HomeRecommend/HomeRecommend.vue
@@ -9,7 +9,7 @@
     <go-writer-button />
 
     <div class="page-state">
-      {{ currentPage }}
+      {{ isLoadMore? currentPage: '亲，数据已经加载完咯' }}
     </div>
   </div>
 </template>
@@ -33,6 +33,7 @@ export default {
     return {
       postList: [],
       currentPage: 0,
+      isLoadMore: true, // 是否有必须尝试请求更多的数据
     };
   },
   methods: {
@@ -45,10 +46,13 @@ export default {
       console.log(res, this.currentPage);
       if (res.status === 404) {
         this.$toast.show("没有数据了", 2000);
+        // 该页没有数据了，以后也不会有了，你不用调用我了，一滴也没有了
+        this.isLoadMore = false
         return;
       } else if (res.status === 200) {
         this.postList.push(...res.data.post);
         this.$toast.show("数据请求成功" + this.currentPage, 2000);
+        // 该页有数据，下一页可能还有数据，你可以试一试调用我
         return;
       }
     },
@@ -66,9 +70,11 @@ export default {
 
       let distance = 220; //距离底部还有220的时候，开始触发；
 
-      if (scrollTop + clientHeight >= scrollHeight - distance) {
+      if (scrollTop + clientHeight >= scrollHeight - distance && this.isLoadMore) {
         console.log("到底了，开始加载数据");
         // 加载数据
+        // 判断本页是否有数据，如果本页已经没有数据了那么下页也不会有了，也犯不着调用代码了
+
         this.loadmore();
       }
     }, 800),
