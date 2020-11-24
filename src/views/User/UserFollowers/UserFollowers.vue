245ccdae635b7eb87847c6d2450d812c58045c30
cemcoe <chemxcode@gmail.com>
2020-11-24 09:40:15 +0800

    feat: 关注页面和粉丝页面数据获取

--- src/views/User/UserFollowers/UserFollowers.vue
+++ src/views/User/UserFollowers/UserFollowers.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    展示用户粉丝列表
-    {{$route.params.uid}}
+    <ul>
+      <li v-for="item in userFollowers" :key="item._id">
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { listfollower } from "network/user";
 export default {
+  data() {
+    return {
+      userFollowers: [],
+    };
+  },
 
-}
+  methods: {
+    async getUserFollowers() {
+      const uid = this.$route.params.uid;
+      const result = await listfollower(uid);
+      this.userFollowers = result.data.followers
+    },
+  },
+  mounted() {
+    this.getUserFollowers();
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/views/User/UserFollowing/UserFollowing.vue
+++ src/views/User/UserFollowing/UserFollowing.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    展示用户关注列表
-    {{$route.params.uid}}
+    <ul>
+      <li v-for="item in userFollowing" :key="item._id">
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { listfollowingUser } from "network/user";
 export default {
+  data() {
+    return {
+      userFollowing: [],
+    };
+  },
 
-}
+  methods: {
+    async getUserFollowing() {
+      const uid = this.$route.params.uid;
+      const result = await listfollowingUser(uid);
+      this.userFollowing = result.data.following;
+    },
+  },
+  mounted() {
+    this.getUserFollowing();
+  },
+};
 </script>
 
 <style>
-
 </style>
