99948d85b9008de84557081182f61e1b1148292e
cemcoe <chemxcode@gmail.com>
2020-11-26 19:16:49 +0800

    feat: 展示我的公开文章和私密文章

--- src/views/My/MyPost/MyPost.vue
+++ src/views/My/MyPost/MyPost.vue
@@ -8,19 +8,32 @@
         <div class="info" slot="center">我的文章</div>
       </nav-bar>
     </div>
-    <div class="tabcontrol"></div>
+    <div class="tabcontrol">
+      <div
+        v-for="tab in tabs"
+        @click="changeStatus(tab)"
+        :key="tab.id"
+        :class="{ on: tab.status === status }"
+      >
+        {{ tab.title }}
+      </div>
+    </div>
 
-    <div class="content">
-      mypost // 获取用户公开文章， // 获取用户私密文章，条件展示
-      {{ $store.state.user._id }}--{{publicPost}}
+    <hr />
 
-      <hr>
-      <hr>
-      <hr>
-
-      {{privatePost}}
+    <div class="content"  v-if="publicPost.length">
+      <div class="public" v-show="status === 'public'">
+        <post-list :postList="publicPost"></post-list>
+      </div>
 
 
+      <div class="private" v-show="status === 'private'">
+        <post-list :postList="privatePost"></post-list>
+      </div>
+    </div>
+
+    <div v-else>
+      <loading></loading>
     </div>
   </div>
 </template>
@@ -28,6 +41,8 @@
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import CemIcon from "components/common/icon/Icon";
+import PostList from "components/content/postList/PostList";
+import Loading from "components/common/loading/Loading";
 
 import { getUserPosts } from "network/user";
 
@@ -35,33 +50,66 @@ export default {
   components: {
     NavBar,
     CemIcon,
+    PostList,
+    Loading,
   },
 
   data() {
     return {
       publicPost: [],
       privatePost: [],
+      status: "public",
+      tabs: [
+        { id: 1, title: "公开文章", status: "public" },
+        { id: 2, title: "私密文章", status: "private" },
+      ],
     };
   },
   methods: {
-    async getPublicPosts (uid) {
-      const res = await getUserPosts(uid, 'public');
-      this.publicPost = res.data
+    async getPublicPosts(uid) {
+      const res = await getUserPosts(uid, "public");
+      this.publicPost = res.data;
     },
 
-    async getPrivatePosts (uid) {
-      const res = await getUserPosts(uid, 'private');
-      this.privatePost = res.data
+    async getPrivatePosts(uid) {
+      const res = await getUserPosts(uid, "private");
+      this.privatePost = res.data;
+    },
+
+    changeStatus(tab) {
+      if (this.status === tab.status) {
+        return;
+      }
+      this.status = tab.status;
+      console.log(this.status);
     },
   },
 
   created() {
-    const uid = this.$store.state.user._id
-    this.getPublicPosts(uid)
-    this.getPrivatePosts(uid)
+    const uid = this.$store.state.user._id;
+    this.getPublicPosts(uid);
+    this.getPrivatePosts(uid);
   },
 };
 </script>
 
-<style>
+<style scoped>
+.tabcontrol {
+  margin-top: 10px;
+  display: flex;
+
+
+}
+.tabcontrol div {
+  flex: 1;
+  text-align: center;
+  line-height: 20px;
+}
+.tabcontrol .on {
+  color: #f40;
+}
+
+.content .on {
+  background-color: #666;
+}
 </style>
