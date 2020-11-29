0bd52555d3592c0df73bef2d31903747c1deeea8
cemcoe <chemxcode@gmail.com>
2020-11-29 10:03:33 +0800

    feat: 获取摄像头视频流数据

--- src/views/Scan/Scan.vue
+++ src/views/Scan/Scan.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-    scan
+    <div>扫描功能</div>
+
+    <button @click="change">前后切换</button>
+
+    <div><video></video></div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      front: false,
+    };
+  },
+  methods: {
+    change() {
+      this.front = !this.front;
+      console.log(this.front);
+    },
+  },
+  created() {
+    var constraints = {
+      video: {
+        facingMode: this.front ? "user" : "environment",
+        audio: true,
+        video: { width: 1280, height: 720 },
+      },
+    };
 
-}
+    // 想要获取一个最接近 1280x720 的相机分辨率
+    // const constraints = {  };
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (mediaStream) {
+        const video = document.querySelector("video");
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function (e) {
+          video.play();
+        };
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+      }); // 总是在最后检查错误
+  },
+};
 </script>
 
 <style>
-
 </style>
