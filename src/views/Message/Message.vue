7c7e21098d634e91ef120a8cbb9badab3ad7eca2
cemcoe <chemxcode@gmail.com>
2020-12-07 20:15:33 +0800

    feat: 测试一下Notification API

--- src/views/Message/Message.vue
+++ src/views/Message/Message.vue
@@ -9,6 +9,11 @@
         <router-link :to="'/room/' + room._id"> {{ room._id }}</router-link>
       </li>
     </ul>
+
+    <div>
+      <button @click="getNotificationPermission">获取通知权限</button>
+      <button @click="notifyMe">通知</button>
+    </div>
   </div>
 </template>
 
@@ -39,6 +44,36 @@ export default {
       const res = await listRooms(this.$store.state.user._id);
       console.log(res, "--------------");
       this.rooms = res.data.rooms;
+    },
+
+    getNotificationPermission() {
+      let promise = Notification.requestPermission();
+    },
+
+    notifyMe() {
+      // 先检查浏览器是否支持
+      if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+      }
+
+      // 检查用户是否同意接受通知
+      else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Hi there!");
+      }
+
+      // 否则我们需要向用户获取权限
+      else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+          // 如果用户接受权限，我们就可以发起一条消息
+          if (permission === "granted") {
+            var notification = new Notification("Hi there!");
+          }
+        });
+      }
+
+      // 最后，如果执行到这里，说明用户已经拒绝对相关通知进行授权
+      // 出于尊重，我们不应该再打扰他们了
     },
   },
 };
