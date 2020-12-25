7ffbefa9eca0ddc41956450794854d134a53f572
cemcoe <chemxcode@gmail.com>
2020-12-25 17:33:19 +0800

    feat: 实现创建简单帖子的功能

--- src/views/Island/IslandDetail.vue
+++ src/views/Island/IslandDetail.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>{{ island.name }} === {{ island.abstract }}</div>
+  <div>
+    <div class="island-info">{{ island.name }} === {{ island.abstract }}</div>
+
+    <div class="create">
+      <form>
+        <label for="content">帖子</label>
+        <input id="content" v-model="islandPost" type="text" />
+      </form>
+      <button @click="createNewIslandPost">创建新帖子</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import { getIslandDetail } from "network/island";
+import { createIslandPost } from "network/islandPost";
+
 export default {
   data() {
     return {
       island: {},
+      islandPost: "",
     };
   },
   created() {
@@ -16,8 +29,26 @@ export default {
       this.island = res.data.island;
     });
   },
+  methods: {
+    async createNewIslandPost() {
+      const islandPost = {
+        content: this.islandPost,
+      };
+      const res = await createIslandPost(
+        this.$route.params.islandId,
+        islandPost
+      );
+      if (res.status === 200) {
+        this.$toast.show("帖子创建成功");
+        this.islandPost = "";
+      }
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+input {
+  border: 1px solid #000;
+}
 </style>
