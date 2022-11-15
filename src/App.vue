9b48ee9e836731f07a622291fbe8ea51c5ccdbd1
cemcoe <chemxcode@gmail.com>
2022-11-15 17:11:00 +0800

    feat: TabBar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import TabBar from "./components/TabBar/TabBar.vue";
 </script>
 
 <template>
   <RouterView />
+  <TabBar v-if="!$route.meta.hideTabBar" />
 </template>
 
 <style scoped></style>
---
+++ src/components/TabBar/TabBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { tabbarRoutes } from "@/router/tabbarRoutes";
+import { getAssetURL } from "@/utils/getAssetsImg";
+</script>
+
+<template>
+  <div class="container">
+    <van-tabbar route active-color="#ff9845">
+      <template v-for="(item, index) in tabbarRoutes" :key="index">
+        <van-tabbar-item replace :to="item.path">
+          <span>{{ item.meta.text }}</span>
+          <template #icon="props">
+            <img
+              :src="
+                props.active
+                  ? getAssetURL(item.meta.imageActive)
+                  : getAssetURL(item.meta.image)
+              "
+            />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: var(--van-tabbar-height);
+}
+img {
+  height: 30px;
+}
+</style>
