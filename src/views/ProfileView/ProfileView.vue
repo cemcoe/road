3b7b4da3d78ffd85cca405550e1153021772e1a3
cemcoe <chemxcode@gmail.com>
2022-11-17 08:00:57 +0800

    refactor: div LoginCard from ProfileView

--- src/views/ProfileView/ProfileView.vue
+++ src/views/ProfileView/ProfileView.vue
@@ -1,44 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { storeToRefs } from "pinia";
-import useLoginStore from "@/stores/login";
-const loginStore = useLoginStore();
-
-const { isLogin } = storeToRefs(loginStore);
-
-const isLoading = ref(false);
-const clickLoginButton = () => {
-  isLoading.value = true;
-  loginStore.loginAction();
-};
+import LoginCard from "./components/LoginCard.vue";
 </script>
 
 <template>
-  <div class="profile">
-    <div class="login" v-if="!isLogin">
-      <van-button
-        size="normal"
-        color="linear-gradient(to right, #ff6034, #ee0a24)"
-        type="primary"
-        @click="clickLoginButton"
-        :disabled="isLoading"
-        >一键登录</van-button
-      >
-    </div>
-  </div>
+  <LoginCard></LoginCard>
 </template>
 
-<style scoped>
-.login {
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - var(--van-tabbar-height));
-  justify-content: center;
-  align-items: center;
-}
-
-.login .info {
-  margin-bottom: 20px;
-  font-size: 20px;
-}
-</style>
+<style scoped></style>
---
+++ src/views/ProfileView/components/LoginCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import useLoginStore from "@/stores/login";
+const loginStore = useLoginStore();
+
+const { isLogin } = storeToRefs(loginStore);
+
+const isLoading = ref(false);
+const clickLoginButton = () => {
+  isLoading.value = true;
+  loginStore.loginAction();
+};
+</script>
+
+<template>
+  <div class="login" v-if="!isLogin">
+    <van-button
+      size="normal"
+      color="linear-gradient(to right, #ff6034, #ee0a24)"
+      type="primary"
+      @click="clickLoginButton"
+      :disabled="isLoading"
+      >一键登录</van-button
+    >
+  </div>
+</template>
+
+<style scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - var(--van-tabbar-height));
+  justify-content: center;
+  align-items: center;
+}
+
+.login .info {
+  margin-bottom: 20px;
+  font-size: 20px;
+}
+</style>
