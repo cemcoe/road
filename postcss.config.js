ddf387cf903e22b22a91cc0e403dcfb69ab351b5
cemcoe <chemxcode@gmail.com>
2023-01-13 12:50:29 +0800

    feat: disable postcss-px-to-viewport

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-undef
-module.exports = {
-  plugins: {
-    "postcss-px-to-viewport": {
-      viewportWidth: 375,
-    },
-  },
-};
+// module.exports = {
+//   plugins: {
+//     "postcss-px-to-viewport": {
+//       viewportWidth: 375,
+//     },
+//   },
+// };
