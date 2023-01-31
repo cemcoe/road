9f82bcc6de4e5d878d76c25673086c70d543e6e9
cemcoe <chemxcode@gmail.com>
2023-01-31 08:10:08 +0800

    chore: import Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -17,6 +20,21 @@ console.log(
 );
 
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SentryDSN,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ["localhost", "road.cemcoe.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 app.use(createPinia());
 app.use(router);
