b56a233c8dc9c4a88ffd1fcb922cd0d3b73f6dbb
cemcoe <chemxcode@gmail.com>
2023-02-03 14:11:32 +0800

    feat: PostDetail popup

--- src/views/PostDetail/PostDetail.vue
+++ src/views/PostDetail/PostDetail.vue
@@ -63,18 +63,40 @@ onBeforeUnmount(() => postStore.initPostDetail());
       v-model:show="show"
       position="bottom"
       round
-      :style="{ height: '30%', padding: '10px' }"
+      closeable
+      close-icon="close"
+      :style="{
+        // height: '30%',
+        padding: '20px 10px',
+        background: 'rgb(250, 247, 247)',
+      }"
     >
-      <van-cell-group>
-        <van-cell @click="edit" title="编辑" is-link />
-        <van-cell title="Cell title" is-link value="Content" />
-        <van-cell
-          title="Cell title"
-          is-link
-          arrow-direction="down"
-          value="Content"
-        />
-      </van-cell-group>
+      <van-space direction="vertical" fill>
+        <van-space direction="vertical" fill>
+          <h3 style="margin-left: 10px">{{ postDetail?.title }}</h3>
+          <p
+            style="
+              margin-left: 10px;
+              margin-bottom: 16px;
+              font: 14px;
+              opacity: 0.5;
+            "
+          >
+            作者信息：{{ postDetail?.author?.name }}
+          </p>
+        </van-space>
+
+        <van-grid :gutter="10" :border="false" clickable>
+          <van-grid-item icon="share" text="分享" />
+          <van-grid-item icon="comment" text="发私信" />
+          <van-grid-item icon="service" text="举报" />
+          <van-grid-item icon="lock" text="拉黑" />
+        </van-grid>
+
+        <van-cell-group>
+          <van-cell title="文章状态" value="公开可读" />
+        </van-cell-group>
+      </van-space>
     </van-popup>
   </div>
 </template>
@@ -88,5 +110,9 @@ onBeforeUnmount(() => postStore.initPostDetail());
   padding: 10px;
   font-size: 20px;
   font-weight: 700;
+}
+
+.post :deep(.van-grid-item .van-grid-item__content) {
+  border-radius: 10px;
 }
 </style>
