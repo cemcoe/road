77215bb616f98897436ae448869f3629e03236ff
cemcoe <chemxcode@gmail.com>
2023-02-03 15:40:11 +0800

    refactor: tabbar style

--- src/components/TabBar/TabBar.vue
+++ src/components/TabBar/TabBar.vue
@@ -1,40 +1,27 @@
 <script setup lang="ts">
 import { tabbarRoutes } from "@/router/tabbarRoutes";
-// import { getAssetURL } from "@/utils/getAssetsImg";
 </script>
 
 <template>
-  <div class="container">
-    <van-tabbar route active-color="#ff9845">
-      <template v-for="(item, index) in tabbarRoutes" :key="index">
-        <van-tabbar-item replace :to="item.path">
-          <span>{{ item.meta.text }}</span>
-
-          <template #icon>
-            <van-icon
-              class-prefix="my-icon"
-              size="30"
-              :name="item.meta.iconName"
-            />
-            <!-- <img
-              width="30"
-              height="30"
-              alt="icon"
-              :src="
-                props.active
-                  ? getAssetURL(item.meta.imageActive)
-                  : getAssetURL(item.meta.image)
-              "
-            /> -->
-          </template>
-        </van-tabbar-item>
-      </template>
-    </van-tabbar>
-  </div>
+  <van-tabbar
+    route
+    inactive-color="rgb(165 165 165)"
+    active-color="#000"
+    placeholder
+  >
+    <template v-for="(item, index) in tabbarRoutes" :key="index">
+      <van-tabbar-item replace :to="item.path">
+        <span style="font-size: 14px">{{ item.meta.text }}</span>
+        <template #icon>
+          <van-icon
+            class-prefix="my-icon"
+            size="20"
+            :name="item.meta.iconName"
+          />
+        </template>
+      </van-tabbar-item>
+    </template>
+  </van-tabbar>
 </template>
 
-<style scoped>
-.container {
-  height: var(--van-tabbar-height);
-}
-</style>
+<style scoped></style>
