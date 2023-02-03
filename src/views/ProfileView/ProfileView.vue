ae643b13cbeb1b8f802bbfee04365c985b88fd91
cemcoe <chemxcode@gmail.com>
2023-02-03 17:11:21 +0800

    feat: Profile touch event

--- src/views/ProfileView/ProfileView.vue
+++ src/views/ProfileView/ProfileView.vue
@@ -36,90 +36,113 @@ onMounted(async () => {
     postList.value = data.postList;
   }
 });
+
+let touchStartClientX = 0;
+let touchEndClientX = 0;
+
+function handelTouchStart(event: TouchEvent) {
+  touchStartClientX = event.changedTouches[0].clientX;
+}
+
+function handelTouchEnd(event: TouchEvent) {
+  touchEndClientX = event.changedTouches[0].clientX;
+  if (touchEndClientX - touchStartClientX < -10) {
+    console.log("向左滑动");
+    showRight.value = true;
+  } else if (touchEndClientX - touchStartClientX > 10) {
+    console.log("向右滑动");
+    showRight.value = false;
+  }
+}
 </script>
 
 <template>
-  <LoginCard></LoginCard>
-  <div v-if="isLogin">
-    <div class="header">
-      <van-nav-bar title="我的" left-text="Back" left-arrow>
-        <template #left>
-          <!-- <van-icon name="scan" color="#000" size="20" /> -->
-        </template>
-        <template #right>
-          <van-icon
-            @click="handelClick"
-            name="wap-nav"
-            color="#000"
-            size="20"
-          />
-        </template>
-      </van-nav-bar>
-    </div>
-
-    <van-popup
-      v-model:show="showRight"
-      position="right"
-      :style="{ width: '70%', height: '100%' }"
-    >
-      <div class="settings">
-        <van-cell-group :border="false">
-          <van-cell
-            :border="false"
-            title="清除缓存"
-            is-link
-            @click="loginStore.logoutAction"
-          />
-          <van-cell :border="false" title="换账号登录" is-link to="/login" />
-        </van-cell-group>
-        <van-cell-group :border="false">
-          <van-cell
-            :border="false"
-            title="GitHub"
-            is-link
-            url="https://github.com/cemcoe/xbook"
-          />
-          <van-cell
-            :border="false"
-            title="Twitter"
-            is-link
-            url="https://twitter.com/cem_coe"
-          />
-        </van-cell-group>
+  <div @touchstart="handelTouchStart" @touchend="handelTouchEnd">
+    <LoginCard></LoginCard>
+    <div v-if="isLogin">
+      <div class="header">
+        <van-nav-bar title="我的" left-text="Back" left-arrow>
+          <template #left>
+            <!-- <van-icon name="scan" color="#000" size="20" /> -->
+          </template>
+          <template #right>
+            <van-icon
+              @click="handelClick"
+              name="wap-nav"
+              color="#000"
+              size="20"
+            />
+          </template>
+        </van-nav-bar>
       </div>
-    </van-popup>
 
-    <div class="card user">
-      <div class="user-info">
-        <div class="avatar" @click="goAuthPage(`/u/${userInfo.id}`)">
-          <img :src="userInfo.avatar" alt="" srcset="" />
+      <van-popup
+        v-model:show="showRight"
+        position="right"
+        :style="{ width: '70%', height: '100%' }"
+      >
+        <div class="settings">
+          <van-cell-group :border="false">
+            <van-cell
+              :border="false"
+              title="清除缓存"
+              is-link
+              @click="loginStore.logoutAction"
+            />
+            <van-cell :border="false" title="换账号登录" is-link to="/login" />
+          </van-cell-group>
+          <van-cell-group :border="false">
+            <van-cell
+              :border="false"
+              title="GitHub"
+              is-link
+              url="https://github.com/cemcoe/xbook"
+            />
+            <van-cell
+              :border="false"
+              title="Twitter"
+              is-link
+              url="https://twitter.com/cem_coe"
+            />
+          </van-cell-group>
         </div>
-        <div class="info">
-          <div class="name">
-            <div class="user-info" @click="goAuthPage(`/u/${userInfo.id}`)">
-              {{ userInfo.name || "请登录" }}
+      </van-popup>
+
+      <div class="card user">
+        <div class="user-info">
+          <div class="avatar" @click="goAuthPage(`/u/${userInfo.id}`)">
+            <img :src="userInfo.avatar" alt="" srcset="" />
+          </div>
+          <div class="info">
+            <div class="name">
+              <div class="user-info" @click="goAuthPage(`/u/${userInfo.id}`)">
+                {{ userInfo.name || "请登录" }}
+              </div>
+            </div>
+            <div class="bottom-line">
+              {{ userInfo.bio }}
             </div>
           </div>
-          <div class="bottom-line">
-            {{ userInfo.bio }}
-          </div>
         </div>
       </div>
-    </div>
 
-    <van-tabs v-model:active="activeName">
-      <van-tab name="post" title="文章"></van-tab>
-      <van-tab name="more" title="更多"></van-tab>
-      <van-tab name="more" title="占喂"></van-tab>
-      <van-tab name="more" title="站位"></van-tab>
-    </van-tabs>
+      <van-tabs v-model:active="activeName">
+        <van-tab name="post" title="文章"></van-tab>
+        <van-tab name="more" title="更多"></van-tab>
+        <van-tab name="more" title="占喂"></van-tab>
+        <van-tab name="more" title="站位"></van-tab>
+      </van-tabs>
 
-    <div class="content">
-      <div v-show="activeName === 'post'">
-        <PostList v-if="postList.length !== 0" :postList="postList"></PostList>
+      <div class="content">
+        <div v-show="activeName === 'post'">
+          <PostList
+            v-if="postList.length !== 0"
+            :postList="postList"
+          ></PostList>
+        </div>
+
+        <div v-show="activeName === 'more'" class="more">more</div>
       </div>
-
-      <div v-show="activeName === 'more'" class="more">more</div>
     </div>
   </div>
 </template>
